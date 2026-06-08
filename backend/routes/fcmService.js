ce2c89945fb6f77c9ba36ed977a73885d66906ee
LamCongDanh <taigaxda@gmail.com>
2026-06-08 23:35:12 +0700

    Setup FCM 2

--- backend/routes/fcmService.js
+++ backend/routes/fcmService.js
@@ -1,17 +1,24 @@
 import admin from "firebase-admin";
 
-export const sendNotification = async (tokens, title, body) => {
+export const sendNotification = async (tokens, title, body, data = {}) => {
+  if (!tokens || tokens.length === 0) return;
+
   const message = {
     notification: {
       title,
       body,
+    },
+    data: {
+      ...data,
     },
     tokens: tokens,
   };
 
   try {
     const res = await admin.messaging().sendEachForMulticast(message);
+
     console.log("Thành công:", res.successCount);
+
     res.responses.forEach((r, i) => {
       if (!r.success) {
         console.log("Token lỗi:", tokens[i]);
@@ -19,6 +26,6 @@ export const sendNotification = async (tokens, title, body) => {
     });
 
   } catch (err) {
-    console.error("Lỗi FCM:", err);
+    console.error("🔥 Lỗi FCM:", err);
   }
 };
--- backend/routes/firebase.js
+++ backend/routes/firebase.js
@@ -1,9 +1,19 @@
+import dotenv from "dotenv/config";
 import admin from "firebase-admin";
-import serviceAccount from "./google-services.json" assert {type: json};
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
+import admin from "firebase-admin";
+import fs from "fs";
+
+let serviceAccount;
+
+if (process.env.FIREBASE_KEY) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+} else {
+  serviceAccount = JSON.parse(
+    fs.readFileSync("./ggfcmkey.json", "utf8")
+  );
 }
 
-export default admin;
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
